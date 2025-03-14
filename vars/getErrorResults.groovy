8f9d15aaac49920ca85069383e35a48983fc293c
Andrei Volkau <andrey.volkov@myoffice.team>
2025-03-14 19:12:52 +0300

    jenkins

--- vars/getErrorResults.groovy
+++ vars/getErrorResults.groovy
@@ -1,9 +1,9 @@
 import org.jenkinsci.plugins.workflow.support.visualization.table.FlowGraphTable
 
 void call() {
-    def results = getErrorResults().collect {
+    def results = "Build errors:<br>" + getErrorResults().collect {
         """<a href="${it.url}">${it.name}: ${it.error}</a>"""
-    }.join("\n")
+    }.join("<br>")
 
     if (currentBuild.description == null || currentBuild.description == "null") {
         currentBuild.description = results
