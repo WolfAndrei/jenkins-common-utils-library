a9471cf9d0ad1b14b6ea647902e31d30f44a7776
Andrei Volkau <andrey.volkov@myoffice.team>
2025-03-14 17:13:35 +0300

    jenkins

--- vars/getStepResults.groovy
+++ vars/getStepResults.groovy
@@ -1,13 +1,4 @@
-import hudson.model.FreeStyleBuild
-import hudson.model.Run
-import io.jenkins.blueocean.listeners.NodeDownstreamBuildAction
-import org.jenkinsci.plugins.workflow.graph.FlowGraphWalker
-import org.jenkinsci.plugins.workflow.graph.FlowNode
-import org.jenkinsci.plugins.workflow.graphanalysis.DepthFirstScanner
-import org.jenkinsci.plugins.workflow.graphanalysis.FlowNodeVisitor
-import org.jenkinsci.plugins.workflow.job.WorkflowJob
 import org.jenkinsci.plugins.workflow.support.actions.LogStorageAction
-import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 import org.jenkinsci.plugins.workflow.support.visualization.table.FlowGraphTable
 
 List<Map> call() {
@@ -20,38 +11,27 @@ List<Map> getStepResults() {
     FlowGraphTable t = new FlowGraphTable(currentBuild.rawBuild.execution)
     t.build()
     for (def row in t.rows) {
-        println(row.displayName)
-        println(row.durationMillis)
-
         if (row.node.error) {
             def nodeInfo = [
                 'name': "${row.node.displayName}",
                 'url': "${env.JENKINS_URL}${row.node.url}",
+                'displayName': "${row.node.error.displayName}",
                 'error': "${row.node.error.error}",
-                'downstream': [:]
+                'logText': ""
             ]
 
-            if (row.node.getAction(LogStorageAction)) {
+            def storageAction = row.node.getAction(LogStorageAction)
+            def output = new ByteArrayOutputStream()
+
+            if (storageAction) {
+                storageAction.logText.writeLogTo(0, output)
+
+                nodeInfo.logText = "${output.toString()}"
                 nodeInfo.url += 'log/'
             }
 
-//            for (def entry in getDownStreamJobAndBuildNumber(row.node)) {
-//                Run<?, ?> run = Jenkins.instance.getItemByFullName(entry.key).getLastBuild()
-//                 nodeInfo.downstream["${entry.key}-${entry.value}"] =""
-//            }
             result << nodeInfo
         }
     }
     return result
-}
-
-Map getDownStreamJobAndBuildNumber(def node) {
-    Map downStreamJobsAndBuilds = [:]
-    for (def action in node.getActions(NodeDownstreamBuildAction)) {
-        def result = (action.link =~ /.*\/(?!\/)(.*)\/runs\/(.*)\//).findAll()
-        if (result) {
-            downStreamJobsAndBuilds[result[0][1]] = result[0][2]
-        }
-    }
-    return downStreamJobsAndBuilds
 }
