4b5a93c3ff26b784509cbebddc6592dc92e51d69
Andrei Volkau <andrey.volkov@myoffice.team>
2025-03-14 15:06:50 +0300

    jenkins

--- vars/getStepResults.groovy
+++ vars/getStepResults.groovy
@@ -2,6 +2,7 @@ import hudson.model.FreeStyleBuild
 import hudson.model.Run
 import io.jenkins.blueocean.listeners.NodeDownstreamBuildAction
 import org.jenkinsci.plugins.workflow.graph.FlowGraphWalker
+import org.jenkinsci.plugins.workflow.graph.FlowNode
 import org.jenkinsci.plugins.workflow.graphanalysis.DepthFirstScanner
 import org.jenkinsci.plugins.workflow.graphanalysis.FlowNodeVisitor
 import org.jenkinsci.plugins.workflow.job.WorkflowJob
@@ -16,37 +17,31 @@ List<Map> call() {
 List<Map> getStepResults() {
     def result = []
 
-    FlowGraphWalker walker = new FlowGraphWalker(currentBuild.rawBuild.execution)
+    FlowGraphTable t = new FlowGraphTable(currentBuild.rawBuild.execution)
+    t.build()
+    for (def row in t.rows) {
+        println(row.displayName)
+        println(row.durationMillis)
 
-    walker.iterator().forEachRemaining {
-        println(it.displayName)
-        println(it.url)
-    }
+        if (row.node.error) {
+            def nodeInfo = [
+                'name': "${row.node.displayName}",
+                'url': "${env.JENKINS_URL}${row.node.url}",
+                'error': "${row.node.error.error}",
+                'downstream': [:]
+            ]
 
-//    FlowGraphTable t = new FlowGraphTable()
-//    t.build()
-//    for (def row in t.rows) {
-//
-//
-//        if (row.node.error) {
-//            def nodeInfo = [
-//                'name': "${row.node.displayName}",
-//                'url': "${env.JENKINS_URL}${row.node.url}",
-//                'error': "${row.node.error.error}",
-//                'downstream': [:]
-//            ]
-//
-//            if (row.node.getAction(LogStorageAction)) {
-//                nodeInfo.url += 'log/'
+            if (row.node.getAction(LogStorageAction)) {
+                nodeInfo.url += 'log/'
+            }
+
+//            for (def entry in getDownStreamJobAndBuildNumber(row.node)) {
+//                Run<?, ?> run = Jenkins.instance.getItemByFullName(entry.key).getLastBuild()
+//                 nodeInfo.downstream["${entry.key}-${entry.value}"] =""
 //            }
-//
-////            for (def entry in getDownStreamJobAndBuildNumber(row.node)) {
-////                Run<?, ?> run = Jenkins.instance.getItemByFullName(entry.key).getLastBuild()
-////                 nodeInfo.downstream["${entry.key}-${entry.value}"] =""
-////            }
-//            result << nodeInfo
-//        }
-//    }
+            result << nodeInfo
+        }
+    }
     return result
 }
 
